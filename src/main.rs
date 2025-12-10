d53a67129bb4bda12dfe2bb2648dcfe545bddc4d
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2025-12-10 11:22:27 -0800

    defining main types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+
+struct Node {
+    children: Vec<Node>,
+    node_type: NodeType,
+}
+
+enum NodeType {
+    Text(String),
+    Element(ElementData),
+}
+
+
+struct ElementData {
+    tag_name: String,
+    attrs: AttrMap,
+}
+
+type AttrMap = HashMap<String, String>;
+
+fn text(data: String) -> Node {
+    Node { children: Vec::new(), node_type: NodeType::Text(data) }
+}
+
+pub fn elem(tag_name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children,
+        node_type: NodeType::Element(ElementData { tag_name, attrs })
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
